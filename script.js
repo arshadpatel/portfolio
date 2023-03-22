aaf41c4fff38142a709bfe81c120a2a7858f8f23
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-03-22 16:33:58 +0530

    Updated project's section and added new design to website

---
+++ script.js
@@ -0,0 +1,35 @@
+function portfolio(){
+    window.location.href="https://arshadpatel.github.io/portfolio/";
+}
+
+function digitalClock(){
+    window.location.href="https://arshadpatel.github.io/digital-clock/";
+}
+
+function ticTacToe(){
+    window.location.href="https://arshadpatel.github.io/tic-tac-toe/";
+}
+
+function attendanceAnalyzer(){
+    window.location.href="https://arshadpatel.github.io/attendance-analyzer/";
+}
+
+function bankApplication(){
+    window.location.href="https://arshadpatel.github.io/bank-application/";
+}
+
+function logRegForm(){
+    window.location.href="https://github.com/arshadpatel/log-reg-form";
+}
+
+function sgpaCalc(){
+    window.location.href="https://arshadpatel.github.io/sgpa-calculator/";
+}
+
+function hackerrank(){
+    window.location.href="https://arshadpatel.github.io/hackerrank-clone/";
+}
+
+function simpleInterestCalc(){
+    window.location.href="https://arshadpatel.github.io/simple-interest-calculator/";
+}
