89e7b6418b04812d69dd158aebf8d7c13e52db8b
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-06-21 00:55:15 +0530

    Adding changes to logo, profile picture, background color and adding new skills

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-function portfolio(){
-    window.location.href="https://arshadpatel.github.io/portfolio/";
+function portfolio() {
+    window.location.href = "https://arshadpatel.github.io/portfolio/";
 }
 
-function digitalClock(){
-    window.location.href="https://arshadpatel.github.io/digital-clock/";
+function digitalClock() {
+    window.location.href = "https://arshadpatel.github.io/digital-clock/";
 }
 
-function ticTacToe(){
-    window.location.href="https://arshadpatel.github.io/tic-tac-toe/";
+function ticTacToe() {
+    window.location.href = "https://arshadpatel.github.io/tic-tac-toe/";
 }
 
-function attendanceAnalyzer(){
-    window.location.href="https://arshadpatel.github.io/attendance-analyzer/";
+function attendanceAnalyzer() {
+    window.location.href = "https://arshadpatel.github.io/attendance-analyzer/";
 }
 
-function bankApplication(){
-    window.location.href="https://arshadpatel.github.io/bank-application/";
+function bankApplication() {
+    window.location.href = "https://arshadpatel.github.io/bank-application/";
 }
 
-function logRegForm(){
-    window.location.href="https://github.com/arshadpatel/log-reg-form";
+function logRegForm() {
+    window.location.href = "https://github.com/arshadpatel/log-reg-form";
 }
 
-function sgpaCalc(){
-    window.location.href="https://arshadpatel.github.io/sgpa-calculator/";
+function sgpaCalc() {
+    window.location.href = "https://arshadpatel.github.io/sgpa-calculator/";
 }
 
-function hackerrank(){
-    window.location.href="https://arshadpatel.github.io/hackerrank-clone/";
+function hackerrank() {
+    window.location.href = "https://arshadpatel.github.io/hackerrank-clone/";
 }
 
-function simpleInterestCalc(){
-    window.location.href="https://arshadpatel.github.io/simple-interest-calculator/";
+function simpleInterestCalc() {
+    window.location.href = "https://arshadpatel.github.io/simple-interest-calculator/";
 }
+
+var typed = new Typed('#element', {
+    strings: ['Student', 'Developer', 'Programming Enthusiast'],
+    typeSpeed: 50,
+});
